11c72db88a151fb3d3a74944c90115b4005d0dbb
Matthew Shin <mshin1995@gmail.com>
2019-02-14 02:35:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length == 0
+  if array.length == 1
     return "#{array[0]}"
   elsif array.length == 2 
-    return array.join("and")
+    return array.join(" and ")
   end
 end
